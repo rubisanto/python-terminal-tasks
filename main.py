863c4b8a1ea3ec033cd16fae5092a43b5cd5965d
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-03-17 10:42:27 +0100

    json-writting

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+
+
 def afficher_menu():
     print("Menu principal :")
     print("1. Ajouter une tâche")
@@ -7,6 +10,24 @@ def afficher_menu():
     print("5. Rechercher une tâche")
     print("6. Marquer une tâche comme terminée")
     print("7. Quitter le programme")
+
+
+NOM_FICHIER = "taches.json"
+
+
+def lire_taches():
+    try:
+        with open(NOM_FICHIER, mode='r', encoding='utf-8') as fichier:
+            taches = json.load(fichier)
+    except FileNotFoundError:
+        taches = []
+
+    return taches
+
+
+def ecrire_taches(taches):
+    with open(NOM_FICHIER, mode='w', encoding='utf-8') as fichier:
+        json.dump(taches, fichier, ensure_ascii=False, indent=4)
 
 
 def main():
